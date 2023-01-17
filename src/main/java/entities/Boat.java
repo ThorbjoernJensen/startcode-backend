add01790cd44d3feb14e43c4efe9115004cdc946
Thorbjørn <cph-tj308@cphbusiness.dk>
2023-01-17 09:25:01 +0100

    ready to go

--- src/main/java/entities/Boat.java
+++ src/main/java/entities/Boat.java
@@ -1,10 +1,6 @@
 package entities;
 
-import dtos.BoatDTO;
-
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "boat")
@@ -13,108 +9,43 @@ public class Boat {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "boat_id", nullable = true)
     private Long id;
-    private String brand;
-    private String model;
-    private String name;
-    private String image;
-    @ManyToMany(mappedBy = "boats")
-    private Set<Owner> owners = new HashSet<>();
-    @ManyToOne //(cascade = { CascadeType.PERSIST, CascadeType.MERGE}) //owning side
-    private Harbour harbour;
+//    private String brand;
+//    private String model;
+//    private String name;
+//    private String image;
+//
+//
+//
+//    @ManyToMany(mappedBy = "boats")
+//    private Set<Owner> owners = new HashSet<>();
+//    @ManyToOne //(cascade = { CascadeType.PERSIST, CascadeType.MERGE}) //owning side
+//    private Harbour harbour;
 
     public Boat() {
     }
 
-    public Boat(BoatDTO boatDTO) {
-        if(boatDTO.getId() != null){
-            this.id = boatDTO.getId();
-        }
-        this.brand = boatDTO.getBrand();
-        this.model = boatDTO.getModel();
-        this.name = boatDTO.getName();
-        this.image = boatDTO.getImage();
-        this.harbour = new Harbour(boatDTO.getHarbour().getId(),
-                boatDTO.getHarbour().getName(),
-                boatDTO.getHarbour().getAddress(),
-                boatDTO.getHarbour().getCapacity());
-//        Ifølge domænediagram må owners ikke være tom, men...
-        if(boatDTO.getOwners() != null) {
-            boatDTO.getOwners().forEach(ownerDTO -> {
-                this.addOwner(new Owner(ownerDTO.getName(), ownerDTO.getAddress(), ownerDTO.getPhone()));
+//    public Boat(BoatDTO boatDTO) {
+//        if(boatDTO.getId() != null){
+//            this.id = boatDTO.getId();
+//        }
+//        this.brand = boatDTO.getBrand();
+//        this.model = boatDTO.getModel();
+//        this.name = boatDTO.getName();
+//        this.image = boatDTO.getImage();
+//        this.harbour = new Harbour(boatDTO.getHarbour().getId(),
+//                boatDTO.getHarbour().getName(),
+//                boatDTO.getHarbour().getAddress(),
+//                boatDTO.getHarbour().getCapacity());
+////        Ifølge domænediagram må owners ikke være tom, men...
+//        if(boatDTO.getOwners() != null) {
+//            boatDTO.getOwners().forEach(ownerDTO -> {
+//                this.addOwner(new Owner(ownerDTO.getName(), ownerDTO.getAddress(), ownerDTO.getPhone()));
+//
+//            });
+//        }
+//    }
 
-            });
-        }
-    }
 
-    public Boat(String brand, String model, String name, String image/*, Harbour harbour*/) {
-        this.brand = brand;
-        this.model = model;
-        this.name = name;
-        this.image = image;
-//        this.harbour = harbour;
 
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String make) {
-        this.model = make;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public Set<Owner> getOwners() {
-        return owners;
-    }
-
-    public void setOwners(Set<Owner> owners) {
-        this.owners = owners;
-    }
-
-    public void addOwner(Owner owner) {
-        this.owners.add(owner);
-        owner.addBoat(this);
-
-    }
-
-    public Harbour getHarbour() {
-        return harbour;
-    }
-
-    public void setHarbour(Harbour harbour) {
-        this.harbour = harbour;
-//        harbour.addBoat(this);
-    }
 }
--- src/main/java/rest/APIResource.java
+++ src/main/java/rest/APIResource.java
@@ -2,25 +2,21 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import dtos.BoatDTO;
-import dtos.HarbourDTO;
-import dtos.OwnerDTO;
 import facades.APIFacade;
 import facades.Populator;
 import utils.EMF_Creator;
 
 import javax.annotation.security.DeclareRoles;
 import javax.persistence.EntityManagerFactory;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
-@Path("boat")
+@Path("boatstuesday")
 @DeclareRoles({"user", "admin"})
 public class APIResource {
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
@@ -41,94 +37,85 @@ public class APIResource {
         return "{\"msg\":\"Hello boatsman\"}";
     }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("owner")
-//    @RolesAllowed({"user"})
-    public String getAllOwners() {
-        Set<OwnerDTO> ownerDTOSet = FACADE.getAllOwners();
-        return GSON.toJson(ownerDTOSet);
-
-    }
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("harbour")
-//    @RolesAllowed({"user"})
-    public String getAllHarbours() {
-        Set<HarbourDTO> harbourDTOSet = FACADE.getAllHarbours();
-        System.out.println("vi var i harbour endpoint");
-//        List<HarbourDTO> harbourDTOList = new ArrayList<>();
-//        harbourDTOList.addAll(harbourDTOSet);
-//        return GSON.toJson(harbourDTOList);
-        return GSON.toJson(harbourDTOSet);
-    }
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("boat")
-//    @RolesAllowed({"user"})
-    public String getAllBoats() {
-        Set<BoatDTO> boatDTOSet = FACADE.getAllBoats();
-        List<BoatDTO> boatDTOList = new ArrayList<>();
-        boatDTOList.addAll(boatDTOSet);
-        return GSON.toJson(boatDTOList);
-    }
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("harbour2")
-//    @RolesAllowed({"user"})
-    public String getAllHarbours2() {
-        Set<HarbourDTO> harbourDTOSet = FACADE.getAllHarbours2();
-
-//        harbourDTOList.addAll(HarbourDTOSet);
-        return GSON.toJson(harbourDTOSet);
-    }
-
-    @POST
-    @Consumes("application/json")
-    @Produces("application/json")
-    @Path("boat")
-    public String createBoat(String boatJSON) {
-        System.out.println("her har vi boatJSON:" + boatJSON);
-        BoatDTO newBoatDTO = GSON.fromJson(boatJSON, BoatDTO.class);
-        System.out.printf("her er vi kommet til efter GSON");
-//        Boat newBoat = new Boat(newBoatDTO);
-        BoatDTO createdBoatDTO = FACADE.createBoat(newBoatDTO);
-        return GSON.toJson(createdBoatDTO);
-
-    }
-
-    @PUT
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("boat/{id}")
-    public String editBoat(@PathParam("id") String id, String boatJSON) {
-        BoatDTO newBoatDTO = GSON.fromJson(boatJSON, BoatDTO.class);
-//        TODO validate input
-        BoatDTO editedBoatDTO = FACADE.editBoat(newBoatDTO);
-        return GSON.toJson(editedBoatDTO);
-    }
-
-
-//            PUT
-///tomcat/CA1/api/ca1/person/{id}
-//    person (2)
-//    person (2)
-//(e4)
-//    DELETE
-///tomcat/CA1/api/ca1/person/{id}
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("populate")
-//    @RolesAllowed({"admin"})
-    public String populateDB() {
-        populator.populate();
-        return "{\"msg\":\"DB populated\"}";
-
-    }
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Path("owner")
+////    @RolesAllowed({"user"})
+//    public String getAllOwners() {
+//        Set<OwnerDTO> ownerDTOSet = FACADE.getAllOwners();
+//        return GSON.toJson(ownerDTOSet);
+//
+//    }
+//
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Path("harbour")
+////    @RolesAllowed({"user"})
+//    public String getAllHarbours() {
+//        Set<HarbourDTO> harbourDTOSet = FACADE.getAllHarbours();
+//        System.out.println("vi var i harbour endpoint");
+////        List<HarbourDTO> harbourDTOList = new ArrayList<>();
+////        harbourDTOList.addAll(harbourDTOSet);
+////        return GSON.toJson(harbourDTOList);
+//        return GSON.toJson(harbourDTOSet);
+//    }
+//
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Path("boat")
+////    @RolesAllowed({"user"})
+//    public String getAllBoats() {
+//        Set<BoatDTO> boatDTOSet = FACADE.getAllBoats();
+//        List<BoatDTO> boatDTOList = new ArrayList<>();
+//        boatDTOList.addAll(boatDTOSet);
+//        return GSON.toJson(boatDTOList);
+//    }
+//
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Path("harbour2")
+////    @RolesAllowed({"user"})
+//    public String getAllHarbours2() {
+//        Set<HarbourDTO> harbourDTOSet = FACADE.getAllHarbours2();
+//
+////        harbourDTOList.addAll(HarbourDTOSet);
+//        return GSON.toJson(harbourDTOSet);
+//    }
+//
+//    @POST
+//    @Consumes("application/json")
+//    @Produces("application/json")
+//    @Path("boat")
+//    public String createBoat(String boatJSON) {
+//        System.out.println("her har vi boatJSON:" + boatJSON);
+//        BoatDTO newBoatDTO = GSON.fromJson(boatJSON, BoatDTO.class);
+//        System.out.printf("her er vi kommet til efter GSON");
+////        Boat newBoat = new Boat(newBoatDTO);
+//        BoatDTO createdBoatDTO = FACADE.createBoat(newBoatDTO);
+//        return GSON.toJson(createdBoatDTO);
+//
+//    }
+//
+//    @PUT
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Path("boat/{id}")
+//    public String editBoat(@PathParam("id") String id, String boatJSON) {
+//        BoatDTO newBoatDTO = GSON.fromJson(boatJSON, BoatDTO.class);
+////        TODO validate input
+//        BoatDTO editedBoatDTO = FACADE.editBoat(newBoatDTO);
+//        return GSON.toJson(editedBoatDTO);
+//    }
+//
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Path("populate")
+////    @RolesAllowed({"admin"})
+//    public String populateDB() {
+//        populator.populate();
+//        return "{\"msg\":\"DB populated\"}";
+//
+//    }
 
 
 }
